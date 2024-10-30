eb88aec4986bb74617364cce70853e85fc855249
datanico <nicolascorbi2002@gmail.com>
2024-10-30 11:49:12 +0100

    formas basicas

--- sketch.js
+++ sketch.js
@@ -4,4 +4,10 @@ function setup() {
   
   function draw() {
     background(220);
+    ellipse(mouseX,mouseY,20)
+    rect(width/2, height/2, mouseX)
+    triangle (0, height, width/2, 0, width, height)
+    line (0, 0, width, height)
+    console.log(frameCount%6)
+    ellipse(frameCount%width, 0, 100)
   }
