9f683916aee296014442199f3811b041c9eb5cff
datanico <nicolascorbi2002@gmail.com>
2024-11-05 11:01:24 +0100

    practica_palabras

--- sketch.js
+++ sketch.js
@@ -1,22 +1,33 @@
+//let string1 = "Hola"
+//let string2 = "Mundo"
+const vocales = "aeiou".split("")
+const consonantes = "qwrtypsdfghjklñxcvbnm".split("")
+console.log(vocales,consonantes)
+
+function crearPalabra(n_silabas)
+{
+    let palabra = ""
+    //console.log(vocales, consonantes)
+    for(let i = 0; i < n_silabas; i++){
+        
+        palabra += random(consonantes) + random(vocales)
+    }
+   
+   // console.log(palabra)
+    return palabra 
+}
+// let lista = ["hola", "mundo", "!"]
+
 function setup() {
     createCanvas(400, 400);
+    frameRate(1)
     background(220);
   }
   
   function draw() {
+   // const string_final = lista[0] + " " + lista[1] + " " + lista[2]
+    const string_final = crearPalabra(random(1)) 
     
-    console.log(sin(frameCount/20)* 100)
-
-    const velocidad = 100
-    const amplitud = 100
-    const posX = width/2
-    const posY = height/2
-
-    //ellipse(cos(frameCount/20)* 100 + 200, frameCount, 1)
-
-    //ellipse(sin(frameCount/20)* 100 + 200, frameCount, 1)
-
-    ellipse(noise(frameCount/velocidad)* amplitud + posX, frameCount, 1)
-    ellipse(noise(frameCount/velocidad)* amplitud + posY, frameCount, 1)
-   
+    text(crearPalabra(random(5)),random(width), random(height));
+    
   }
